8522d01f4e1b64c5f10a284b563cfad349462356
BaileyV <baileyvarness@gmail.com>
2019-10-23 17:03:23 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,48 @@
+$(document).ready(function(){
+    $("#img-one").click(function() {
+        $("#img-one").hide();
+    })
+
+    $("#img-two").click(function() {
+        $("#img-two").hide();
+    })
+
+    $("#img-three").click(function() {
+        $("#img-three").hide();
+    })
+
+    $("#img-four").click(function() {
+        $("#img-four").hide();
+    })
+
+    $("#img-five").click(function() {
+        $("#img-five").hide();
+    })
+
+    $("#img-six").click(function() {
+        $("#img-six").hide();
+    })
+
+    $("#img-seven").click(function() {
+        $("#img-seven").hide();
+    })
+
+    $("#img-eight").click(function() {
+        $("#img-eight").hide();
+    })
+
+    $("button").click(function() {
+        $("img").show();
+    })
+
+
+
+
+
+
+
+
+
+});
+
+
